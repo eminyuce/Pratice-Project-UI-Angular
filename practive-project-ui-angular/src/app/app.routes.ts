30c820f882a2a7cac9a20ee55274d61794293494
eminyuce <eminyuce@gmail.com>
2024-12-25 12:53:28 -0500

    route app

--- practive-project-ui-angular/src/app/app.routes.ts
+++ practive-project-ui-angular/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { AcquUserEntityComponent } from './acqu-user-entity/acqu-user-entity.component';
 
 export const routes: Routes = [
-    { path: 'acqu-user-entity', component: AcquUserEntityComponent }
+    { path: '', component: AcquUserEntityComponent }
   ];
